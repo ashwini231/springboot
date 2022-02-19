2fca681cc7e390395a54637a73df937217cdeaaf
HP <ashwinims2001@gmail.com>
2022-02-19 18:41:43 +0530

    Initial

---
+++ src/main/java/com/javaweb/springboot/service/EmployeeService.java
@@ -0,0 +1,11 @@
+package com.javaweb.springboot.service;
+
+import java.util.List;
+
+import org.springframework.data.domain.Page;
+import com.javaweb.springboot.model.*;
+
+public interface EmployeeService {
+	List<Employee> getAllEmployees();
+	
+}
